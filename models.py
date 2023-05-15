86bbffe5a14eb9aeb5e547eccc60e7e6cec5ba08
sigae <sigae@protonmail.com>
2023-05-15 16:31:26 +0900

    [Feat] article 모델 생성

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+
 from database import Base
 
 
@@ -10,3 +11,13 @@ class User(Base):
     nickname = Column(String, nullable=False)
     password = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
+
+
+class Article(Base):
+    __tablename__ = "article"
+
+    id = Column(Integer, primary_key=True)
+    amount = Column(Integer, unique=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    create_at = Column(DateTime, nullable=False)
+    detail = Column(Text, unique=True, nullable=False)
