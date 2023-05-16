82dce288d26b9dd1052022f48b42ffa5200388aa
sigae <sigae@protonmail.com>
2023-05-16 15:12:17 +0900

    [Feat] 라우터 메타데이터 작성

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from starlette.middleware.cors import CORSMiddleware
 
 from router import user, article
 
-tags_metadata = [
-    {
-        "name": "AUTH",
-        "description": "인증, 등록 등을 다룹니다.",
-    },
-]
+tags_metadata = [{
+    "name": "AUTH",
+    "description": "인증, 등록 등을 다룹니다.",
+}, {
+    "name": "Article",
+    "description": "게시글 CRUD를 다룹니다."
+}]
 
-app = FastAPI(
-    title="Ugeo API",
-    openapi_tags=tags_metadata
-)
+app = FastAPI(title="Ugeo API", openapi_tags=tags_metadata)
 
 origins = ["*"]
 
@@ -21,8 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "PUT", "DELETE",
-                   "PATCH"],
+    allow_methods=["GET", "PUT", "DELETE", "PATCH"],
     allow_headers=["*"],
 )
 
