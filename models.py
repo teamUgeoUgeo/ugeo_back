671a6e3ccb6ad9a9d3f6a6204f805de72ac8335f
sigae <sigae@protonmail.com>
2023-05-16 14:05:19 +0900

    [feat] model 업데이트
    [Feat] model 추가

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -21,3 +22,4 @@ class Article(Base):
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
     create_at = Column(DateTime, nullable=False)
     detail = Column(Text, unique=True, nullable=False)
+    user = relationship("User", backref="article_users")
