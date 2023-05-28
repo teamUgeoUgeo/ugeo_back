989dff960c9d8ea094d54e75a8f657b7783c3c44
SiGae <sigae@protonmail.com>
2023-05-29 01:23:24 +0900

    [Feat] article table scheme 수정

--- models.py
+++ models.py
@@ -18,8 +18,8 @@ class Article(Base):
     __tablename__ = "article"
 
     id = Column(Integer, primary_key=True)
-    amount = Column(Integer, unique=True, nullable=False)
+    amount = Column(Integer, nullable=False)
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
     create_at = Column(DateTime, nullable=False)
-    detail = Column(Text, unique=True, nullable=False)
+    detail = Column(Text, nullable=False)
     user = relationship("User", backref="article_users")
