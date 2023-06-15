301d384d19419b2d7fc88ebe7a5bd8001bbf80e2
sigae <sigae@protonmail.com>
2023-06-15 11:23:49 +0900

    [Feat] DB생성유무 확인 추가

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
+
 from config import env
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{env.DB_ID}:{env.DB_PASSWORD}@{env.DB_URL}/ugeougeo"
 
+if not database_exists(SQLALCHEMY_DATABASE_URL):
+    create_database(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, echo=True
 )
